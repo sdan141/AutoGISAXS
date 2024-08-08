5387f34dfedc739398b3cdd6c2286b6613116f9d
shachar-dan <shachar.dan@desy.de>
2024-08-08 10:00:24 +0200

    update

---
+++ deep_learning/MLPnet.py
@@ -0,0 +1,53 @@
+from tensorflow.keras import layers, models, backend
+#from keras_applications import imagenet_utils
+from deep_learning import algorithm
+
+class MLPNet(algorithm.AlgorithmBase):
+    TYPE = None
+
+    def __init__(self, input_shape, parameter, output_units):
+        self.output_units = output_units
+
+        morphology = ['radius','distance','sigma_radius','omage_distance']
+        if not parameter['distribution']:
+            morphology = ['radius','distance']
+        if parameter['morphology'] != 'all':
+            morphology = [key for key in morphology if key in parameter['morphology']]
+
+        if self.TYPE == "MLP2":
+            self.activations = 'lr'
+            self.model = self.build_mlp2(input_shape=input_shape, morphology=morphology)
+        elif self.TYPE == "MLP3":
+            self.model = self.build_mlp3(input_shape=input_shape, morphology=morphology)
+        algorithm.AlgorithmBase.__init__(self, model=self.model, parameter=parameter, morphology=morphology)
+
+    def build_mlp2(self, input_shape, morphology, hidden=1024):
+        print('input_shape to build_model:', input_shape)
+        #input_shape = imagenet_utils._obtain_input_shape(input_shape, default_size=224, min_size=32, data_format=backend.image_data_format(), require_flatten=True)
+        image = layers.Input(shape=input_shape,name="image")
+        x = layers.Flatten()(image)
+        x = layers.Dense(units=hidden, name='fc' + str(hidden))(x)
+        x = layers.LeakyReLU(alpha=0.03)(x) if self.activation[0]=='l' else layers.ReLU()(x)
+
+        outputs = []
+        if 'distance' in morphology:
+            distance = layers.Dense(units=self.output_units['distance'], activation="softmax", name="distance")(x)
+            outputs.append(distance)
+        if 'omega_distance' in morphology:
+            omega_distance = layers.Dense(units=self.output_units['omega_distance'], activation="softmax", name="omega_distance")(x)
+            outputs.append(omega_distance)
+        if 'radius' in morphology:
+            radius = layers.Dense(units=self.output_units['radius'], activation="softmax", name="radius")(x)
+            outputs.append(radius)
+        if 'sigma_radius' in morphology:
+            sigma_radius = layers.Dense(units=self.output_units['sigma_radius'], activation="softmax", name="sigma_radius")(x)
+            outputs.append(sigma_radius)
+        # create model
+        model = models.Model(inputs=image, outputs=outputs, name=self.TYPE+"_"+self.activations)
+        return model
+
+
+class MLP2(MLPNet):
+    TYPE = "MLP2"
+    def __init__(self, input_shape, parameter, output_units):
+        MLPNet.__init__(self, input_shape=input_shape, parameter=parameter, output_units=output_units)
---
+++ deep_learning/label_coder.py
@@ -0,0 +1,57 @@
+
+import numpy as np
+
+class LabelCoder:
+
+    def __init__(self, output_units, morphology='all', distr=False):
+        self.n_radius_units = output_units['radius']
+        self.n_distance_units = output_units['distance']
+        self.n_sigma_units = output_units['sigma']
+        self.n_omega_units = output_units['omega']
+
+        self.radius = np.arange(1.2, 1.2+self.n_radius_units*0.1, 0.1)
+        self.distance = np.arange(3.8, 3.8+self.n_distance_units*0.1, 0.1)
+        self.sigma_radius = np.arange(0.1, 0.1+self.n_sigma_units*0.01, 0.01)
+        self.omega_distance= np.arange(0.1, 0.1+self.n_omega_units*0.01, 0.01)
+
+        self.prob_space = {'radius':self.radius, 'distance': self.distance, 'sigma_radius': self.sigma_radius, 'omega_radius': self.omega_distance}
+        '''
+        if distr else ['radius', 'distance']
+        if morphology != 'all':
+            self.keys = [key in self.keys if morphology in key]
+        '''
+    def create_labels(self, target_values, key=None):
+        '''
+        parameters
+        return
+        '''
+        labels = []
+        for target in target_values:
+             labels.append(self.get_label(target, key))
+        return labels
+
+    def get_label(self, target, key):
+        label = [1 if round(target,2)==round(param,2) else 0 for param in self.probe_space[key]]
+        assert sum(label) > 1.0, "One-hot label sum larger than one"
+        assert sum(label) < 1.0, "One-hot label sum smaller than one"
+        return label
+
+    def coldbatch_maximum(self, labels, key):
+        values = []
+        for label in labels:
+            values.append(self.get_value_max(label, key))
+        return values
+
+    def coldbatch_average(self, labels, key):
+        values = []
+        for label in labels:
+            values.append(self.get_value_avg(label, key))
+        return values
+
+    def get_value_max(self, label, key):
+        value = self.prob_space[key][np.argmax(label)]
+        return value
+
+    def get_value_avg(self, label, key):
+        value = sum(self.prob_space[key]*label)
+        return value
